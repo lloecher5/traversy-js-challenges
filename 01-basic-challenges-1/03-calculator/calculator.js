3d0d66fe1fcba73bde896f480ca295e26a53d6a3
Luke Loecher <ljloecher@gmail.com>
2023-10-11 12:31:35 -0400

    finished first 3 challenges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,13 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+    if(op === '+'){
+        return num1 + num2
+    } else if (op === '-'){
+        return num1 - num2
+    } else if (op === '*'){
+        return num1 * num2
+    } else if (op === '/'){
+        return num1 / num2
+    }
+}
 
 module.exports = calculator;
